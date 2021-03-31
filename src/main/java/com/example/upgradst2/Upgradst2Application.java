66e4037a458a0cb6f19f22388185d36501341d45
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-03-31 17:42:02 +0530

    projet

---
+++ src/main/java/com/example/upgradst2/Upgradst2Application.java
@@ -0,0 +1,13 @@
+package com.example.upgradst2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Upgradst2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Upgradst2Application.class, args);
+	}
+
+}
